cf1ea9455a9ed5d500af252626931c728ccb0d07
김동환 <dongdong@gimdonghwan-ui-MacBook-Pro.local>
2017-02-09 21:52:35 +0900

    Initial commit

---
+++ app/src/main/java/graph/atedu/dongdong/gittest/MainActivity.java
@@ -0,0 +1,13 @@
+package graph.atedu.dongdong.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
